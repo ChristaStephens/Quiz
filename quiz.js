e9ec9c85ae63153379e62d1d4e026d0067a0213c
Christa Stephens <ChristaStephens@outlook.com>
2019-02-11 13:50:18 -0500

    vars for quiz

---
+++ quiz.js
@@ -0,0 +1,24 @@
+let quizContainer = document.getElementById('quiz');
+let resultsContainer = document.getElementById('results');
+let submitButton = document.getElementById('submit');
+
+function buildQuiz() {
+    
+}
+
+function showResult() {
+    
+}
+
+buildQuiz();
+
+// add event listener to show results so that user
+//can see results on click
+
+submitButton.addEventListener('click', showResult);
+
+let myQuestions = [
+    {
+        
+    }
+]
